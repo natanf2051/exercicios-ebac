8e5dbbbbd9f9f698d4eba971c6791f51c0fc89c6
natanf2051 <115187503+natanf2051@users.noreply.github.com>
2022-10-08 00:39:54 -0300

    a

---
+++ tarefa_calculo_media/calcular.java
@@ -0,0 +1,12 @@
+package tarefa_calculo_media;
+
+public class calcular {
+   public static void main(String[] args) {
+     
+    media media = new media();
+
+    media.getnotaFinal();
+
+
+    }
+}
---
+++ tarefa_calculo_media/media.java
@@ -0,0 +1,21 @@
+package tarefa_calculo_media;
+
+public class media {
+    
+    
+    public int n1 = 3;
+    public int n2 = 5;
+    public int n3 = 4;
+    public int n4 = 8;
+
+    public int getnotaFinal(){
+     int nota = this.n1 + this.n2 + this.n3 + this.n4;
+     int total = nota/4;
+     System.out.println(total);
+    return nota;
+   
+    };
+};
+
+
+
