93e58bbc9e8ae46307747dbdc85586d9161acf72
natanf2051 <115187503+natanf2051@users.noreply.github.com>
2022-10-08 12:37:34 -0300

    tarefa 09 Java

---
+++ modulo9/tarefa.java
@@ -0,0 +1,21 @@
+package modulo9;
+
+public class tarefa {
+    
+    public static void main(String[] args){
+    
+        //transformando int em Integer.
+    int num = 1555;
+    System.out.println(num);
+    Integer numb2 = (Integer) num;
+
+    //transformando double rm Integer.
+    double nume = 22.3; 
+    System.out.println(nume);
+    Integer num1 = (int) nume;
+    System.out.println(num1);
+
+    
+    }
+
+}
