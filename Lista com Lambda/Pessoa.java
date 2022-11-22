3b16cffef073282727257287d2667eb9f1dad759
natanf2051 <115187503+natanf2051@users.noreply.github.com>
2022-11-21 21:27:27 -0300

    Lista lambda
    tarefa 22

--- Lista com Lambda/Pessoa.java
+++ Lista com Lambda/Pessoa.java
@@ -1,40 +1,64 @@
+import java.util.List;
 import java.util.Scanner;
 
 public class Pessoa{
+Scanner input = new Scanner(System.in);
 
 private String nome;
 private String sexo;
-Scanner input = new Scanner(System.in);
 
-//public Pessoa (String nome, String sexo){
-//    String name = input.next();
-//    this.nome = name;
-//    String sex = input.next();
-//    this.sexo = sex;
-//
-//}
+
+
+public void setnome(String nome) {
+    String name = input.next();
+    this.nome = name;
+}
+
+public void setSexo(String sexo) {
+    String sex = input.next();
+    this.sexo = sex;
+}
+
+public Pessoa() {
+  this.nome = nome;
+  
+  this.sexo = sexo;
+}
 
 public String getNome() {
-    return nome + " " + sexo;
+    return nome;
 }
 
 public String getSexo() {
     return sexo;
 }
 
+public List<Pessoa> popularPessoa(){
+    Pessoa pess = new Pessoa();
+    
+   System.out.print("digite seu nome: ");
+   pess.setnome(nome);
+   System.out.print("digite seu sexo: ");
+   pess.setSexo(sexo);
 
+    System.out.println("CADASTRE UMA SEGUNDA PESSOA");
 
+   Pessoa pess1 = new Pessoa();
+   System.out.print("digite seu nome: ");
+   pess1.setnome(nome);
+   System.out.print("digite seu sexo: ");
+   pess1.setSexo(sexo);
+ 
 
-public void setDados(String nome, String sexo){
-    String name = input.next();
-    this.nome = name;
-    String sex = input.next();
-    this.sexo = sex;
+   return List.of(pess, pess1);
+
 }
 
 @Override
 public String toString() {
-    return "Pessoa [nome= " + nome + ", sexo= " + sexo + "]";
+    return "Pessoa [nome=" + nome + ", sexo=" + sexo + "]";
 }
+
+
 
 }
